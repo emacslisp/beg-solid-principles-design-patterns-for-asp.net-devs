fe5117ebf4d6fd9c531928db7f7d3e65c13fd38c
emacslisp <java.gdb@gmail.com>
2018-03-31 14:37:42 +1100

    add tool project for all tools using .net core

---
+++ core2.0/ToolProject/ToolProject/ToolProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ToolProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
